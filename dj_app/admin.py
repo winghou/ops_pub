8bae5012bc7c3911e929b39c8ae4d033dd18e29e
zsmlinux <zsmlinux@163.com>
2018-03-05 23:05:07 +0800

    20180305 配置sqlite，测试models.py，测试admin后台

--- dj_app/admin.py
+++ dj_app/admin.py
@@ -2,5 +2,7 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
-
+from dj_app import models
 # Register your models here.
+
+admin.site.register(models.BlogPost)
--- dj_app/views.py
+++ dj_app/views.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.shortcuts import render
+from django.shortcuts import render,render_to_response
 from django.http import HttpResponse
 from django.template import loader
 import os
@@ -13,4 +13,5 @@ def index(request):
 def php_publish(request):
     return render(request, 'php_publish.html')
 def php_command(request):
-    os.system("dir")
+    result = os.system(str(request))
+    return render_to_response("index.html", {"message": result})

04a29e6ea974e89bf98ad07e6e55ae8941d8d8b4
zsmlinux <zsmlinux@163.com>
2018-03-07 22:51:22 +0800

    20180307 测试真实视图，通过读取数据库条目，为模板创建上下文字典，将上下文传递给模板，最后将模板渲染到HTML，通过HTTP来响应HTML

--- dj_app/urls.py
+++ dj_app/urls.py
@@ -7,5 +7,4 @@ urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^index.html$', views.index, name='index'),
     url(r'php_publish.html$', views.php_publish, name='php_publish'),
-    url(r'^php_command/$', views.php_command, name='php_command'),
 ]
--- dj_app/views.py
+++ dj_app/views.py
@@ -3,7 +3,9 @@ from __future__ import unicode_literals
 
 from django.shortcuts import render,render_to_response
 from django.http import HttpResponse
-from django.template import loader
+from django.template import loader, Context
+from dj_app.models import BlogPost
+from datetime import datetime
 import os
 
 # Create your views here.
@@ -11,7 +13,7 @@ def index(request):
     #return HttpResponse('Hello, world.')
     return render(request, 'index.html')
 def php_publish(request):
-    return render(request, 'php_publish.html')
-def php_command(request):
-    result = os.system(str(request))
-    return render_to_response("index.html", {"message": result})
+    posts = BlogPost.objects.all()
+    t = loader.get_template("php_publish.html")
+    c = Context().update({'posts': posts})
+    return HttpResponse(t.render(c))
